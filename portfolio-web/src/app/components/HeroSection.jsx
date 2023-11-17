85a9795a6f3bb65eb84812ac6dfb0eb3f2adbe24
= <=>
2023-11-16 23:40:09 -0300

    creacion de componentes navbar y navlink incluyendo estilos con clases utilitarias de tailwind

--- portfolio-web/src/app/components/HeroSection.jsx
+++ portfolio-web/src/app/components/HeroSection.jsx
@@ -9,7 +9,9 @@ export default function HeroSection() {
         <div className='grid grid-cols-1 sm:grid-cols-12'>
             <div className='col-span-7 place-self-center text-center sm:text-left'>
                 <h1 className="text-white mb-4  text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-purple-500'>Hola, soy {""}</span>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-purple-500'>
+                        Hola, soy {""}
+                    </span>
                     <br/>
                     <TypeAnimation
                     sequence={[
@@ -41,13 +43,13 @@ export default function HeroSection() {
                 </div>
             </div>
             <div className='col-span-5 place-self-center mb-6 mt-4 lg:mt-0'>
-                <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+                <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative'>
                     <Image
                     src= "/images/hero1.png"
                     alt='hero-image'
                     className='absolute tranform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                    width= {500}
-                    height={500}
+                    width= {300}
+                    height={300}
                     />               
                 </div>
             </div>
--- portfolio-web/src/app/page.js
+++ portfolio-web/src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
+      <NavBar />
+      <div className="mt-24 container mx-auto px-12 py-4">
         <HeroSection />
       </div>
     </main>
