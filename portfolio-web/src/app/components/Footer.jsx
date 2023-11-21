671d8944bbe0585ae1cd5ca987bd1656fedacd73
= <=>
2023-11-21 05:04:00 -0300

    añadiendo footer

--- portfolio-web/src/app/components/Footer.jsx
+++ portfolio-web/src/app/components/Footer.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
+      <div className='container p-12 flex justify-between'>
+        <span>Jona</span>
+        <p className='text-slate-600'>
+          &copy; 2023 Todos los derechos reservados.
+      </p>
+      </div>
+    </footer>
   )
 }
