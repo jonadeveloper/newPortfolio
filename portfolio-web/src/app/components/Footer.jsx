da82078102486851d2c4d2e90a2291531ee92517
= <=>
2023-11-26 22:35:13 -0300

    modificando textos en navbar y footer

--- portfolio-web/src/app/components/Footer.jsx
+++ portfolio-web/src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
       <div className='container p-12 flex justify-between'>
-        <span>Jona</span>
+        <span>Jona Code</span>
         <p className='text-slate-500'>
           &copy; 2023 Todos los derechos reservados. | Un sitio creado por Jonatan Vilalva
         </p>
