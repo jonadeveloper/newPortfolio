011b88a090c7a40fa5189def8309b14c94f27354
= <=>
2024-03-11 21:32:38 -0300

    ajustando detalles de mapa

--- portfolio-web/src/app/components/EmailSection.jsx
+++ portfolio-web/src/app/components/EmailSection.jsx
@@ -4,6 +4,7 @@ import GitHubIcon from '../../../public/github-icon.svg'
 import LinkedinIcon from '../../../public/linkedin-icon.svg'
 import EmailIcon from '../../../public/emails-icon.svg'
 import WhatsappIcon from '../../../public/whatsapp-icon.svg'
+import LocationIcon from '../../../public/location-pin-svgrepo-com.svg'
 import Link from 'next/link'
 import Image from 'next/image'
 
@@ -34,11 +35,20 @@ export default function EmailSection() {
                 </Link>
             </div>
         </div>
-        <div>
-        <h5 className='text-xl text-center font-bold text-white my-2 pb-3'>Estoy aqui</h5>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52467.916956628345!2d-58.28791971186763!3d-34.724223118572695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32e38e45f5777%3A0x6a86dac21334524f!2sQuilmes%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1710198661332!5m2!1ses-419!2sar" 
-        className="w-full h-[60vh]">
-        </iframe>
+        <div className='m-auto pt-12'>
+        <h5 className='flex items-center justify-center text-xl text-center font-bold text-white my-2'>
+            Mi localización
+            <Image src={LocationIcon} alt='icon-location' />
+        </h5>
+        <iframe
+        title="Google Maps"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52467.916956628345!2d-58.28791971186763!3d-34.724223118572695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32e38e45f5777%3A0x6a86dac21334524f!2sQuilmes%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1710198661332!5m2!1ses-419!2sar"
+        className='w-[35rem] h-[22rem]'
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+    />
         </div>
     </section>
     
